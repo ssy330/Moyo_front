df030e76f3a02aae5bc262753f813a2fcc46056e
HwangHotae <hotae0321@naver.com>
2025-09-30 21:53:36 +0900

    kakao button 수정

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,5 +1,5 @@
 import logoImg from "@/assets/Moyo_logo.png";
-//import KakaoTalkIcon from "@/assets/KakaoTalkIcon.tsx";
+import KakaoTalkIcon from "@/assets/KakaoTalkIcon.tsx";
 
 // 하단 링크들 스타일 정의
 const linkButtonClass = "hover:underline focus:outline-none cursor-pointer";
@@ -10,7 +10,7 @@ export default function LoginPage() {
     <div className="min-h-screen flex flex-col md:flex-row">
       {/* 왼쪽 영역 - 로고 */}
       <div className="flex flex-1 items-center justify-center p-8 -mt-4 md:-mt-15">
-        <img className="w-80 h-50 bg-gray-300 rounded" />
+        <img className="w-auto h-50  rounded" src={logoImg} />
       </div>
 
       {/* 오른쪽 영역 - 로그인 박스 */}
@@ -30,15 +30,8 @@ export default function LoginPage() {
           </div>
 
           {/* 로그인 버튼 */}
-          <button className="h-12 bg-[#FEE500] rounded mb-3 flex items-center justify-center w-full cursor-pointer gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="black"
-              className="w-5 h-5"
-            >
-              <path d="M12 2C6.48 2 2 5.94 2 10.5c0 2.38 1.33 4.5 3.44 5.94-.15.95-.54 2.6-1.38 3.99-.12.21.08.46.31.39 1.8-.53 3.33-1.49 4.23-2.19 1.07.3 2.2.47 3.4.47 5.52 0 10-3.94 10-8.5S17.52 2 12 2z" />
-            </svg>
+          <button className="h-12 bg-[#FEE500] rounded mb-3 flex items-center justify-center w-full cursor-pointer gap-2 hover:bg-[#F4DC00]">
+            <KakaoTalkIcon />
             카카오계정으로 시작하기
           </button>
 
