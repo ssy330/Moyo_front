2dfa19d63dbc863c1b81fd4633d2e6d950c3126e
HwangHotae <hotae0321@naver.com>
2025-10-28 11:40:52 +0900

    feat: add shadcb/UI, pretterrc

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // src/를 @로 단축
@@ -15,8 +16,8 @@ export default defineConfig({
       // 백엔드 FastAPI 서버로 요청 프록시
       "/api/v1": {
         target: "http://127.0.0.1:8000", // FastAPI 실행 주소
-        changeOrigin: true,              // CORS 회피용
+        changeOrigin: true, // CORS 회피용
       },
     },
   },
-})
+});
